665c55a1469af785f02a6a8e6bb833e7715e0ab4
Youssof Mohamed <90064094+Mos2d@users.noreply.github.com>
2022-11-19 18:57:57 -0500

    added writeHeader

--- ControllerWork/src/com/mcgill/mcgillcv/controller/Generate.java
+++ ControllerWork/src/com/mcgill/mcgillcv/controller/Generate.java
@@ -2,6 +2,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 import model.CV;
 import model.Club;
 import model.Course;
@@ -37,31 +40,29 @@ public class Generate {
     
     
     public static void createHTML() {
-      try {
-        File myObj = new File("C:\\Users\\fyren\\eclipse-workspace\\ControllerWork\\src\\com\\mcgill\\mcgillcv\\controller\\model\\template.html");
-        if (myObj.createNewFile()) {
-          System.out.println("File created: " + myObj.getName());
-        } else {
-          System.out.println("File already exists.");
-        }
-      } catch (IOException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-      }
-      
-      String string = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n\"http://www.w3.org/TR/html4/loose.dtd\">\n<html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n<title>$title</title>\n</head>\n<body>$body\n</body>\n</html>"; 
-      
-      try {
-        
-        // Open given file in append mode by creating an
-        // object of BufferedWriter class
-        BufferedWriter out = new BufferedWriter(
-            new FileWriter("C:\\Users\\fyren\\eclipse-workspace\\ControllerWork\\src\\com\\mcgill\\mcgillcv\\controller\\model\\template.html", true));
+    	File myObj = new File("D:\\Projects\\McGill-CV\\ControllerWork\\index.html");
+    	 String string ="";
+    	try {
+	       string = FileUtils.readFileToString(myObj);
+    	} catch (IOException e) {
 
-        // Writing on output stream
-        out.write(string);
-        // Closing the connection
-        out.close();
+            // Display message when exception occurs
+            System.out.println("exception occurred" + e);
+        }
+	      
+    	string = writeHeader(string);
+    	
+	      try {
+	        
+	        // Open given file in append mode by creating an
+	        // object of BufferedWriter class
+	        BufferedWriter out = new BufferedWriter(
+	            new FileWriter("D:\\Projects\\McGill-CV\\ControllerWork\\template.html", false));
+	
+	        // Writing on output stream
+	        out.write(string);
+	        // Closing the connection
+	        out.close();
     }
 
     // Catch block to handle the exceptions
@@ -70,16 +71,13 @@ public class Generate {
         // Display message when exception occurs
         System.out.println("exception occurred" + e);
     }
-      
-//      String htmlString = FileUtils.readFileToString(htmlTemplateFile);
-//      String title = "New Page";
-//      String body = "This is Body";
-//      htmlString = htmlString.replace("$title", title);
-//      htmlString = htmlString.replace("$body", body);
-//      File newHtmlFile = new File("path/new.html");
-//      FileUtils.writeStringToFile(newHtmlFile, htmlString);
-      
-      
+	      
+	      
+    }
+    
+    public static String writeHeader(String string) {
+    	string = string.replace("$name", cv.getBasicInformation().getName());
+    	return string;
     }
     
     
