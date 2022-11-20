26785ce7abcc01225ebefe595742365ce790adbf
Abdelrahman Ali (Abdul) <abdalrahman2587@gmail.com>
2022-11-19 21:21:30 -0500

    added ui elements to first fragment

--- app/src/main/java/com/mcgill/mcgillcv/FirstFragment.java
+++ app/src/main/java/com/mcgill/mcgillcv/FirstFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,24 +15,24 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.mcgill.mcgillcv.databinding.FragmentFirstBinding;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private FragmentFirstBinding binding;
 
+    private Spinner majorSpinner;
+    private Spinner minorSpinner;
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +40,22 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        majorSpinner = (Spinner) view.findViewById(R.id.major_spinner);
+        ArrayAdapter<CharSequence> majorAdaptor = ArrayAdapter.createFromResource(getContext(),
+                R.array.major, android.R.layout.simple_spinner_item);
+
+        majorAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        majorSpinner.setAdapter(majorAdaptor);
+        majorSpinner.setOnItemSelectedListener(this);
+
+        minorSpinner = (Spinner) view.findViewById(R.id.minor_spinner);
+        ArrayAdapter<CharSequence> minorAdaptor = ArrayAdapter.createFromResource(getContext(),
+                R.array.minor, android.R.layout.simple_spinner_item);
+
+        minorAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minorSpinner.setAdapter(minorAdaptor);
+        minorSpinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -44,4 +64,15 @@ public class FirstFragment extends Fragment {
         binding = null;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selected = (String) parent.getItemAtPosition(position);
+        Toast.makeText(getContext(), selected, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Toast.makeText(getContext(), "dsds", Toast.LENGTH_LONG).show();
+
+    }
 }
